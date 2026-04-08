27c52c30390ca408321cdcd51c1168c4a0263f83
Mamello-09 <>
2026-04-08 09:53:27 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,6 @@
+#define variable and assign Value
+total_cost = 25.00
+discountThreshnold = 20.00
+discountEligible = total_cost >= discountThreshnold
+
+print("Is the purchase eligible for a discount?" , discountEligible )
